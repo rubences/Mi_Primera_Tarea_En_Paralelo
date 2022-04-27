439b580de502aaf36d73a32a97b861c8a9ac8263
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-04-27 13:58:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+from time import sleep
+def scrape(url):
+    print("starting", url)
+    duration = round(random.random(), 3)
+    sleep(duration)
+    print("finished", url, "time taken:", duration, "seconds")
+    return url, duration
