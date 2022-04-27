c42546473b443f5ba6a7f498ee043dc89f2b6d4d
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-04-27 14:02:32 +0000

    Hacer el trabajo secuencialmente

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import random
 from time import sleep
+
+urls = ["a.com", "b.com", "c.com", "d.com"]
+
+
 def scrape(url):
     print("starting", url)
     duration = round(random.random(), 3)
     sleep(duration)
     print("finished", url, "time taken:", duration, "seconds")
     return url, duration
+
+output = []
+for url in urls:
+    result = scrape(url)
+    output.append(result)
